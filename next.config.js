184a44f2ee1c22428f13c52a132408fedb8161d2
Jon <jonathanws18@gmail.com>
2023-10-01 01:34:15 -0600

    updated next config to deploy static

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	// only generate static exports instead of both static and dynamic
+	output: 'export',
 	reactStrictMode: true,
 	// transpile dependencies that show 'unexpected token export' errors
 	transpilePackages: ['react-syntax-highlighter'],
