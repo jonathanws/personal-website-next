c7eb33610b46c41537d95fa522067011508f4710
Jon <jonathanws18@gmail.com>
2023-10-05 14:43:00 -0600

    added fira code to Code components

--- src/components/blog/Code.tsx
+++ src/components/blog/Code.tsx
@@ -4,17 +4,10 @@ import IconButton, { IconButtonProps } from '@mui/material/IconButton'
 import { alpha, darken, lighten, styled, useTheme } from '@mui/material/styles'
 import Tooltip from '@mui/material/Tooltip'
 import copy from 'clipboard-copy'
+import { Fira_Code } from 'next/font/google'
 import { useState } from 'react'
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
-
-interface Props {
-	highlightLines?: number[]
-	language:
-		| 'bash'
-		| 'typescript'
-	text: string[]
-}
 
 // because you can't configure indentation to use tabs...
 export const TAB = '	'
@@ -22,6 +15,11 @@ export const FOUR_SPACES = '    '
 
 // background color of the code blocks.  Unsure how to import, so duplicated here
 const codeBlockBackgroundColor = 'rgb(40, 44, 52)'
+
+const firaCode = Fira_Code({
+	fallback: ['Courier', 'monospace'],
+	subsets: ['latin'],
+})
 
 // IconButton component initialized with styles
 const StyledIconButton = styled(IconButton)<IconButtonProps>(() => ({
@@ -36,6 +34,14 @@ const StyledIconButton = styled(IconButton)<IconButtonProps>(() => ({
 	right: '10px',
 	top: '10px',
 }))
+
+interface Props {
+	highlightLines?: number[]
+	language:
+		| 'bash'
+		| 'typescript'
+	text: string[]
+}
 
 export default function Code({ highlightLines, language, text }: Props) {
 	const [showSuccessIcon, setShowSuccessIcon] = useState(false)
@@ -61,9 +67,16 @@ export default function Code({ highlightLines, language, text }: Props) {
 				</StyledIconButton>
 			</Tooltip>
 			<SyntaxHighlighter
+				codeTagProps={{
+					style: {
+						fontFamily: firaCode.style.fontFamily,
+						fontWeight: 500,
+					},
+				}}
 				customStyle={{
 					borderRadius: 6,
-					lineHeight: '1.75rem',
+					fontSize: 16,
+					lineHeight: '1.5rem',
 					marginBottom: 16,
 					maxWidth: '100vw',
 					padding: 16,
